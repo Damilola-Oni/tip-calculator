eeaaa15ecc4729e9d9e83c3e0a5ba8ff595fe246
Damilola git config --global user.name Damilola <giftedoluwadamilola07@gmail.com>
2020-11-10 12:24:21 +0100

    fix

--- tip calculator/js/script.js
+++ tip calculator/js/script.js
@@ -1,18 +1,18 @@
 const form = document.getElementById('calculator')
-
+// form submit event handler
 form.onsubmit = function(event){
   event.preventDefault()
 
-let howMuch = document.getElementById('bill')
-let tipAmount = document.getElementById('percentage')
-let customerNum = document.getElementById('customer-num')
+const howMuch = document.getElementById('bill')
+const tipAmount = document.getElementById('percentage')
+const customerNum = document.getElementById('customer-num')
 
-let totalBill = document.getElementById('tatal-bill')
-let totalPercentage = document.getElementById('total-percentage')
-let totalPerCustomer = document.getElementById('total-per-customer')
+const totalBill = document.getElementById('total-bill')
+const totalPercentage = document.getElementById('total-percentage')
+const totalPerCustomer = document.getElementById('total-per-customer')
 
   let cost = parseFloat(howMuch.value);
-  let customers =parseInt(customerNum.value);
+  let customers = parseInt(customerNum.value);
 
   let percentage = 0;
   if(tipAmount.value !== ''){
